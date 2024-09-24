597b4430c7fefda7fd103d5e212b8647a403b028
Rui Zhang <641619594@qq.com>
2024-09-24 18:28:00 +0800

    优化月度对账单格式

--- src/views/make/selector/index.vue
+++ src/views/make/selector/index.vue
@@ -5,11 +5,11 @@
 		</el-card>
 
 		<el-card shadow="hover" header="月度对账单" class="mt15">
-			<el-table :data="state.tableData" style="width: 100%">
+			<el-table :data="state.tableData" show-summary style="width: 100%">
 				<el-table-column prop="支付方式" label="资产账户"> </el-table-column>
-				<el-table-column prop="支出" label="支出" align="right"> </el-table-column>
-				<el-table-column prop="收入" label="收入" align="right"> </el-table-column>
-        <el-table-column prop="不计收支" label="不计收支" align="right"> </el-table-column>
+				<el-table-column prop="支出" label="支出(￥)" align="right"> </el-table-column>
+				<el-table-column prop="收入" label="收入(￥)" align="right"> </el-table-column>
+        <el-table-column prop="不计收支" label="不计收支(￥)" align="right"> </el-table-column>
 			</el-table>
 		</el-card>
 
