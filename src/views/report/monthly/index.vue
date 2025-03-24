49f88d02c979fbb2c738cb1dd82aab7524cdfe30
Rui Zhang <641619594@qq.com>
2025-03-24 21:34:18 +0800

    拆分报表管理页面

---
+++ src/views/report/monthly/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<div id="app">
+		<div class="container">
+			<div class="block">
+				<el-date-picker
+					v-model="value3"
+					type="year"
+					placeholder="选择年">
+				</el-date-picker>
+			</div>
+		</div>
+	</div>
+	<div class="selector-container layout-pd">
+    <el-card shadow="hover" header="月度账单" class="mt15">
+      <el-table :data="state.tableData" style="width: 100%">
+        <el-table-column prop="month" label="月份"> </el-table-column>
+        <el-table-column prop="balance" label="余额(￥)" align="right"> </el-table-column>
+        <el-table-column prop="income" label="收入(￥)" align="right"> </el-table-column>
+        <el-table-column prop="expenditure" label="支出(￥)" align="right"> </el-table-column>
+        <el-table-column prop="credit" label="流入(￥)" align="right"> </el-table-column>
+        <el-table-column prop="debit" label="流出(￥)" align="right"> </el-table-column>
+      </el-table>
+    </el-card>
+	</div>
+</template>
+
+<script setup lang="ts" name="makeSelector">
+import { reactive } from 'vue';
+import { useReportApi } from '/@/api/report';
+
+// 定义变量内容
+const state = reactive({
+	tableData: [],
+});
+
+const initTableData = async () => {
+  const { getMonthlyBalance } = useReportApi();
+  // 获取月度账单数据
+  getMonthlyBalance().then((res) => {
+    state.tableData = res;
+  });
+};
+
+initTableData();
+</script>
+
+<script lang="ts">import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+const app = createApp({});
+app.use(ElementPlus);
+</script>
