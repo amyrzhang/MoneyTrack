b33faf1300e7fc43a637e60b5197fc44ca8997ca
Rui Zhang <641619594@qq.com>
2025-04-06 12:39:55 +0800

    feat(report): 月度账单增加日期选择器以筛选数据

--- src/views/report/monthly/index.vue
+++ src/views/report/monthly/index.vue
@@ -1,5 +1,13 @@
 <template>
- b  	<div class="selector-container layout-pd">
+	<div class="selector-container layout-pd">
+		<el-date-picker
+			v-model="value"
+			type="year"
+			format="YYYY"
+			value-format="YYYY"
+			placeholder="选择年"
+			@change="initTableData"
+		></el-date-picker>
     <el-card shadow="hover" header="月度账单" class="mt15">
       <el-table :data="state.tableData" style="width: 100%">
         <el-table-column prop="month" label="月份"> </el-table-column>
@@ -14,18 +22,23 @@
 </template>
 
 <script setup lang="ts" name="makeSelector">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import { useReportApi } from '/@/api/report';
 
 // 定义变量内容
 const state = reactive({
 	tableData: [],
 });
+const value = ref('');
 
 const initTableData = async () => {
   const { getMonthlyBalance } = useReportApi();
+	const params = {
+		time: value.value,
+	};
+
   // 获取月度账单数据
-  getMonthlyBalance().then((res) => {
+  getMonthlyBalance(params).then((res) => {
     state.tableData = res;
   });
 };
